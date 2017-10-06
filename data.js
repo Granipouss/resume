76e4a99edcbe811b4e12f6716b60af2161720b62
Brendan <Granipouss@gmail.com>
2017-10-06 13:44:38 +0200

    Basic Resume

--- data.js
+++ data.js
@@ -1,4 +1,12 @@
 const data = {
-  title: 'My Resume',
-  subtitle: 'I am a Student'
+  title: 'John Doe',
+  subtitle: 'I am a Student',
+  description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pretium nunc ut erat faucibus, eget ultrices felis dapibus. Sed rutrum elementum leo imperdiet fringilla. Duis tortor nunc, elementum quis ligula ac, vulputate placerat nibh.',
+  parts: [{
+    title: 'Part 1',
+    content: 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum in sollicitudin leo, vel fringilla lectus. Curabitur non felis ligula. Integer id dolor augue. Donec et nunc eget libero bibendum cursus. Duis a egestas magna, a euismod enim. Mauris vestibulum ornare ante, eget accumsan eros commodo sed.'
+  }, {
+    title: 'Part 2',
+    content: 'Aenean in tristique sapien, nec vulputate augue. Suspendisse sit amet arcu feugiat, tempor sem at, ornare ligula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Nunc rutrum turpis et nibh hendrerit, et commodo lectus vestibulum. Sed vitae viverra elit, eu rutrum nunc. Pellentesque magna sapien, viverra at tortor sed, cursus rutrum mauris. Nunc pharetra facilisis lacinia. Aenean pulvinar libero in imperdiet lobortis. Proin dui quam, volutpat et imperdiet quis, gravida id magna. Ut eu felis eget erat feugiat feugiat nec ut lorem. Proin ut gravida nunc, volutpat faucibus augue. Donec posuere non magna suscipit imperdiet. Nullam at sapien felis.'
+  }]
 }
