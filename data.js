18003197f0a36150236bfd48bf30f82e186f3584
Brendan <Granipouss@gmail.com>
2018-07-05 07:46:25 +0200

    update

--- data.js
+++ data.js
@@ -1,92 +1,125 @@
 const data = {
   title: 'Brendan DAOUD',
-  subtitle: 'Software Engineering Student',
+  subtitle: 'Ingénieur Logiciel',
   side: {
     info: {
       title: 'Contacts',
       list: [
-        { label: 'Nationality', text: 'French' },
-        { label: 'Address', text: '139, Rue La Fayette<br>75010 Paris<br>FRANCE' },
-        { label: 'Phone Number', text: '+33 6 76 28 23 08' },
+        { label: 'Adresse', text: '139, Rue La Fayette<br>75010 Paris<br>FRANCE' },
+        { label: 'Téléphone', text: '+33 6 76 28 23 08' },
         { label: 'Email', text: 'daoud@ensta.fr' },
         { label: 'Github', text: '<small>github.com/Granipouss<br>github.com/brendaoud</small>' }
       ]
     },
     skills: [{
-      title: 'Languages',
+      title: 'Langues',
       list: [
-        { label: 'French', text: 'Mother tongue' },
-        { label: 'English', text: 'Advanced (TOEIC 935)' },
-        { label: 'Spanish & Japanese', text: 'Beginner' }
+        { label: 'Français', text: 'Langue maternelle' },
+        { label: 'Anglais', text: 'Avancé (TOEIC 935)' },
+        { label: 'Espagnol & Japonais', text: 'Débutant' }
       ]
     }, {
-      title: 'Computer Skills',
+      title: 'Informatique',
       list: [
-        { label: 'Scripting Languages', text: 'C & C++<br>Python & Java<br>NodeJS<br>Basic Ruby' },
-        { label: 'Web Technologies', text: 'JS, HTML5 & CSS3<br>Basic PHP' },
-        { label: 'Other Software', text: 'Microsoft Office Suite<br>Adobe Creative Suite' }
+        { label: 'Langages de Script', text: 'C & C++<br>Python & Java<br>NodeJS<br>bases de Ruby' },
+        { label: 'Technologies Web', text: 'JS ES6, HTML5 & CSS3<br>PHP7' },
+        { label: 'Autres Logiciels', text: 'Microsoft Office Suite<br>Adobe Creative Suite<br>Maîtrise de GIT,<br>Connaissance de MVC et des principaux Designer&nbsp;Patterns' }
       ]
     }]
   },
   main: [
     {
       isTable: true,
-      title: 'Work Experience',
+      title: 'Scolarité',
       list: [{
-        date: 'June - August 2017',
-        label: 'Research Internship at the Computer Science and System Engineering Laboratory of ENSTA',
-        text: 'I used my knowledge in image treatment, statistics and parallel computing to evaluate statistics of natural images that were quite unknown. My Work will be used as a basis for a natural science paper on the way the visual system works. <small>(the code was in C++ for computation and Python for visualization)</small>'
+        date: 'Depuis 2014',
+        label: 'ENSTA ParisTech',
+        text: 'École d’ingénieur<br>Voie Signal, Informatique et Systèmes & Parcours Architecture et Sécurité des Systèmes d’Information - Profils Entrepreneuriat et Intrapreneuriat'
       }, {
-        date: 'May - August 2016',
-        label: 'Internship at Soixante Circuits',
-        text: 'Soixante Circuits is an Interactive Design agency in Paris working for various international companies. After being part of the team developing all promotional applications for Coca Cola during the UEFA Champions League in France, I developed one of the four applications used in an exhibition for the anniversary of Tiffany & Co in China. <small>(some code can be found on github at github.com/brendaoud - it was all made in JavaScript and NodeJS)</small>'
+        date: '2012 - 2014',
+        label: 'Classe Préparatoire',
+        text: 'Lycée Charlemagne, Paris<br>Classe préparatoire mathématiques, physique et informatique'
       }, {
-        date: 'Since 2015',
-        label: 'Developing MLVA website for Université Paris Sud',
-        text: 'The website was old and the code was not maintainable but it was also commonly used by many biologists worldwide. With another student, we remade it from scratch using good practices and well-documented libraries. I am currently ensuring maintenance and adding functionalities. <small>(the website is available at http://microbesgenotyping.i2bc.paris-saclay.fr/ - the code is in PHP and JavaScript)</small>'
-      }, {
-        date: '2014 - 2015',
-        label: 'Websites for school clubs',
-        text: `
-          I had multiple opportunities to use my programming skills to help my school's clubs.
-          I updated the website of the Art Bureau to make it more flexible as the theme is to change every year. <small>(the website is available at http://bda.ensta-paristech.fr - the code is in PHP and JavaScript)</small><br>
-          I updated the website of the art festival <i>les Arts en Scène</i>, I refreshed the front-end to make it more responsive and created a back-end that allows organizers to contact the artists more  easily. <small>(the website is available at http://lesartsenscene.ensta-paristech.fr/ - the code is in PHP and JavaScript)</small>`
-      }, {
-        date: '2014 - 2015',
-        label: 'Managing a school club',
-        text: 'For one year, each month I organized with a team of 15 to 20 people a meal for around 200 people. I was in charge of the cooking and the service.'
+        date: '2012',
+        label: 'Baccalauréat S',
+        text: 'Lycée Victor Hugo, Paris'
       }]
     }, {
       isTable: true,
-      title: 'Education',
+      title: 'Expériences Professionelles',
       list: [{
-        date: 'Since 2014',
-        label: 'ENSTA ParisTech',
-        text: 'Engineering degree program<br>Specialized in Information Systems and Technology'
+        date: 'Avril - Sept. 2018',
+        label: 'Stage de fin d\'étude en cours chez CardMarket à Berlin',
+        text: `Leader européen de la vente de cartes à jouer et à collectionner.<br>
+              Développement de nouvelles fonctionnalités, maintenances et mises à
+              niveau des fonctionnalités actuelles.<br>
+              <small>Utilisation de PHP principalement</small>`
       }, {
-        date: '2012 - 2014',
-        label: 'Preparatory School',
-        text: 'Lycée Charlemagne, Paris<br>Focused on Mathematics, Physics and Computer Sciences<br>Equivalent to a 2-year university degree in Mathematics and mandatory to apply for engineering schools.'
+        date: 'Juin - Août 2017',
+        label: 'Stage de Recherche au laboratoire U2IS de l’ENSTA',
+        text: `Projet de recherche sur les statistiques des images naturelles
+              dans l’Unité d’Informatique et d’Ingénierie Système de l’ENSTA ParisTech<br>
+              Mise en pratique de mes connaissances en traitement d'images, statistiques et
+              calcul en parallèle dans le but d'en apprendre plus sur les caractéristiques
+              des images dites naturelles.<br>
+              <small>Utilisation de Python et C++ pour le traitement d’images en parallèle</small>`
       }, {
-        date: '2012',
-        label: 'Scientific Baccalauréat',
-        text: 'Lycée Victor Hugo, Paris'
+        date: 'Mai - Août 2016',
+        label: 'Stage de développeur chez Soixante circuits',
+        text: `Agence parisienne de design interactif
+            <ul>
+              <li>Participation au développement d'applications utilisées par Coca Cola
+                pour la couverture de l'UEFA en France et en Belgique.</li>
+              <li>Développement d'une exposition interactive pour l'anniversaire de Tiffany
+                & Co en Chine. </li>
+            </ul>
+            <small>Utilisation de Node JS, notamment les librairies Vue.js et Webpack</small>`
+      }, {
+        date: '2015 - 2017',
+        label: 'Site de MLVA pour \'Université Paris Sud',
+        text: `Remise à neuf d'un site participatif de séquençage du génomes de
+              microbes utilisé par des biologistes à travers le monde. Développement
+              sur 3 ans avec un camarade de l'ENSTA, en relation constante avec
+              les chercheurs.<br>
+              <small>Site disponible à http://microbesgenotyping.i2bc.paris-saclay.fr/<br>
+              Utilisation en PHP avec le framework Code Igniter et JS</small>`
+      // }, {
+      //   date: 'Août 2015',
+      //   label: 'Stage dans un hôtel au Japon',
+      //   text: 'Stage linguistique et stage en tant qu\'homme à tout faire dans un hôtel'
+      // }, {
+      //   date: 'Août 2013',
+      //   label: 'Job estival à la Banque de France',
+      //   text: 'Assitant au secrétariat du servcie de contrôle des Assurances et Mutuelles'
+      }]
+    }, {
+      isTable: false,
+      title: 'Vie associative',
+      list: [{
+        label: 'Bureau des Arts',
+        text: `Association étudiante gérant l\'ensemble des activités culturelles
+              et artistiques du campus.<br>
+              Création et gestion du site de billetterie en ligne.`
+      }, {
+        label: 'l\'Anim\'',
+        text: `Organisation de repas pour environ 200 personnes. Gestion de l'équipe (15 à 20 personnes) -
+              Planification du travail en amont et répartition des tâches lors de l'évènement.`
       }]
     }, {
       isTable: false,
       title: 'Hobbies',
       list: [{
-        label: 'Visual Arts',
-        text: 'Drawing and painting and also occasionally photography and film'
+        label: 'Arts Visuels',
+        text: 'Dessin et peinture, visites de musées et d\'expositions, film et séries d\'animations'
       }, {
-        label: 'Computer Science',
-        text: 'Websites, games and various projects'
+        label: 'Informatique',
+        text: 'Sites web et jeux amateurs, projets divers'
       }, {
-        label: 'Sports',
-        text: 'Gymnastics, Swimming'
+        label: 'Sport',
+        text: 'Gymnastique, Natation'
       }, {
-        label: 'Cooking',
-        text: 'Keen pastry chef'
+        label: 'Cuisine',
+        text: 'Pâtisserie principalement'
       }]
     }
   ]
